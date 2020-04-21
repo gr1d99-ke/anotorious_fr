d58e776789c3d3ad446727e24c08267102ec2ae4
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-04-21 17:55:17 +0300

    setup uppy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Dashboard } from "@uppy/react"
+import Uppy from "@uppy/core";
+import AwsS3 from "@uppy/aws-s3";
+import ms from "ms";
+
+const uppy = Uppy({
+  meta: { type: 'avatar' },
+  restrictions: { maxNumberOfFiles: 1 },
+  autoProceed: true
+});
+
+uppy.use(AwsS3, {
+  limit: 2,
+  timeout: ms('1 minute'),
+  companionUrl: process.env.REACT_APP_COMPANION_URL
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Uppy Stuff</header>
+      <Dashboard uppy={uppy}/>
     </div>
   );
 }
